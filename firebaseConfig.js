06da3963476b59d12ba9964a5fdcbd00c253c91c
paozaniktum-code <paozaniktum@gmail.com>
2025-09-26 14:54:51 +0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4onNF2FmdJti7_fX6swoa-cqdq3xojTk",
+  authDomain: "lead-54035.firebaseapp.com",
+  projectId: "lead-54035",
+  storageBucket: "lead-54035.firebasestorage.app",
+  messagingSenderId: "993343538703",
+  appId: "1:993343538703:web:51ba10b9296b4716fa457a",
+  measurementId: "G-KB5ZRKN3T5"
+};
